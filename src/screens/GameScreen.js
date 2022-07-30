111cbd3cfc31d671f4bd8a497dcf24e06aee2ac5
Stan K <motrhelp@stans-air.home>
2022-07-30 17:13:34 +0200

    Fix game window size and introduce flag image

--- src/screens/GameScreen.js
+++ src/screens/GameScreen.js
@@ -1,7 +1,19 @@
+import { Box } from "@mui/material";
 import GuessCountryScreen from "./GuessCountryScreen";
 
 export default function GameScreen() {
     return (
-        <GuessCountryScreen />
+        <Box
+            sx={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                width: "450px",
+                height: "900px",
+            }}
+        >
+            <GuessCountryScreen />
+        </Box >
     );
 }
--- src/screens/GuessCountryScreen.js
+++ src/screens/GuessCountryScreen.js
@@ -2,20 +2,19 @@ import React from "react";
 import { Box, Grid, Typography } from "@mui/material";
 import Hearts from "../features/hearts/Hearts";
 import CountryAutocomplete from "../features/countryAutocomplete/CountryAutocomplete";
+import Shape from "../features/Shape";
 
 export default function GuessCountryScreen() {
 
     return (
         <Grid container
-            align="center" 
+            align="center"
             sx={{
-                height: "100vh",
+                height: "100%",
             }}
         >
             <Grid item xs={12}>
-                <Typography variant="h4">
-                    Title
-                </Typography>
+                <Shape />
             </Grid>
             <Grid item xs={12}>
                 <Hearts />
